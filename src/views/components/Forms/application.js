30cfca5939272e0a4ba702b47221bfe8b67ba215
Idayat <github email address>
2017-06-04 12:00:40 -0400

    add constants for dates

--- src/views/components/Forms/application.js
+++ src/views/components/Forms/application.js
@@ -4,3 +4,19 @@ import { connect } from 'react-redux'
 import DatePicker from 'react-datepicker'
 import moment from 'moment'
 import 'react-datepicker/dist/react-datepicker.css';
+
+const form = reduxForm({
+  form: 'application'
+})
+
+const renderField = field => (
+    <input className="uk-input uk-width-small uk-margin-left uk-margin-right uk-form-controls" {...field.input}/>
+)
+
+const renderCheckbox = field => (
+    <input type="checkbox" className="uk-checkbox uk-margin-left" {...field.input}/>
+)
+
+const renderDatePicker = ({input, placeholder, defaultValue, meta: {touched, error} }) => (
+    <DatePicker {...input} className="uk-input uk-width-small uk-margin-left uk-margin-right uk-form-controls" dateForm="MM/DD/YYYY" selected={input.value ? moment(input.value) : null} />
+);
