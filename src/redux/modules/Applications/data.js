3736c94d14b3d1308e205258f3abaf14279f2537
Idayat <github email address>
2017-06-04 12:51:22 -0400

    add data for form

---
+++ src/redux/modules/Applications/data.js
@@ -0,0 +1,19 @@
+const DATA = [
+  {
+    id: 1,
+    user_id: 1,
+    company: "Google",
+    contact_name: "Larry Page",
+    contact_title: null,
+    date: "05/15/2017",
+    action: "email",
+    first_contact: true,
+    job_title: "Lead Engineer",
+    job_url: null,
+    notes: "Looks promising",
+    complete: false,
+    next_step: "Send Follow up",
+    status: null,
+    created_at: "2017-04-30T15:31:35.185Z",
+    updated_at: "2017-04-30T15:31:35.185Z"
+  },
