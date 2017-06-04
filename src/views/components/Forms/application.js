bc403a2385a843f2efa4f12246078dca813f09bd
Idayat <github email address>
2017-06-04 11:58:13 -0400

    import redux to forms

---
+++ src/views/components/Forms/application.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+import 'react-datepicker/dist/react-datepicker.css';
