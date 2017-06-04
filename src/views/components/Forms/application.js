749598ce680939775d5973b65af100922da973d2
Idayat <github email address>
2017-06-04 12:02:27 -0400

    add information to the form

--- src/views/components/Forms/application.js
+++ src/views/components/Forms/application.js
@@ -20,3 +20,26 @@ const renderCheckbox = field => (
 const renderDatePicker = ({input, placeholder, defaultValue, meta: {touched, error} }) => (
     <DatePicker {...input} className="uk-input uk-width-small uk-margin-left uk-margin-right uk-form-controls" dateForm="MM/DD/YYYY" selected={input.value ? moment(input.value) : null} />
 );
+
+class ApplicationForm extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment()
+    };
+  }
+
+  componentDidMount() {
+    this.handleInitialize()
+  }
+
+  handleInitialize() {
+    const currentApplication = this.props.currentApplication
+    const initData = {
+      "company": currentApplication.company,
+      "date": currentApplication.date,
+      "complete": currentApplication.complete
+    }
+    this.props.initialize(initData)
+  }
