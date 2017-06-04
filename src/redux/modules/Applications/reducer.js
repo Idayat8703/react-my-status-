06715b9961f5d0ca720debe2d50ad1636e28c095
Idayat <github email address>
2017-06-04 12:54:38 -0400

    add reducers for the application

--- src/redux/modules/Applications/reducer.js
+++ src/redux/modules/Applications/reducer.js
@@ -22,3 +22,21 @@ export default (state = initialState, action) => {
           ...state,
           applications: action.applications
         }
+        case 'ADD_APPLICATION':
+          return {
+            ...state,
+            applications: state.applications.concat(action.application)
+          }
+
+        case 'EDIT_APPLICATION':
+          const application = state.applications.filter(a => a.id === action.application.id)[0]
+          const index = state.applications.findIndex(a => a.id === action.application.id)
+          const editedApplication = Object.assign({}, application, action.application)
+          return {
+            applications: [
+                  ...state.applications.slice(0, index),
+                  editedApplication,
+                  ...state.applications.slice(index + 1)
+                ],
+                currentApplication: {}
+            }
