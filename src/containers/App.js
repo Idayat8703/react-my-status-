6ee31a7631e0de745ed0867704b50706ddceefb7
Idayat <github email address>
2017-06-04 11:35:41 -0400

    add test to app

--- src/containers/App.js
+++ src/containers/App.js
@@ -37,7 +37,7 @@ class App extends Component {
       this.props.authenticate(token)
     } else {
       // Ping the API server in case it hasn't been used in 30 inutes and Heroku put it to sleep
-      fetch('http://localhost:3000/api/v1')
+      fetch('http://localhost:3001/api/v1')
     }
   }
   render() {
@@ -76,19 +76,12 @@ const mapStateToProps = (state) => {
   }
 }
 
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    logout,
+    authenticate,
+    authFailure
+  }, dispatch)
 }
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
