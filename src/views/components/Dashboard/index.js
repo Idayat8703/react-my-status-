be3e15db8b11c171c9b0cf8436fc8f1985559491
Idayat <github email address>
2017-06-04 12:26:00 -0400

    add handle new application feature

--- src/views/components/Dashboard/index.js
+++ src/views/components/Dashboard/index.js
@@ -27,3 +27,18 @@ class Dashboard extends Component {
     this.props.clearCurrentApplication()
     this.openModal()
   }
+  handleNewApplication = (data) => {
+    const user_id = this.props.currentUser.id
+    return ApiServices.post("/users/" + user_id + "/applications", data, this.props.token)
+      .then(response => {
+        const { application } = response
+        this.props.addApplication(application)
+        this.props.reset('application')
+        this.closeModal()
+      })
+      .catch((errors) => {
+        console.log(errors)
+        throw new SubmissionError(errors)
+      })
+
+  }
