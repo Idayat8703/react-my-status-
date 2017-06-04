bc78b7f53c01b071809f39a13e3d8ea4339f20ae
Idayat <github email address>
2017-06-04 11:42:06 -0400

    add app onclick to index.js

--- src/views/components/ApplicationRow/index.js
+++ src/views/components/ApplicationRow/index.js
@@ -7,7 +7,7 @@ function ApplicationRow(props){
       props.onDelete(props.user_id, props.application.id)
     }
   }
-  
+
   let contact = ""
   if (props.application.contact_name && props.application.contact_title) {
     contact = props.application.contact_name + " - " + props.application.contact_title
@@ -16,3 +16,16 @@ function ApplicationRow(props){
   } else if (props.application.contact_title) {
     contact = props.application.contact_title
   }
+  return (
+        <tr>
+          <td onClick={handleClick}>{props.application.company}</td>
+          <td onClick={handleClick}>{props.application.job_title}</td>
+          <td onClick={handleClick}>{contact}</td>
+          <td onClick={handleClick}>{props.application.date}</td>
+          <td onClick={handleClick}>{props.application.action}</td>
+          <td onClick={handleClick}>{props.application.notes}</td>
+          <td onClick={handleClick}>{props.application.complete ? 	<span>&#x2713;</span> :	<span>&#x2717;</span> }</td>
+          <td><button className="uk-button uk-button-danger uk-button-small" onClick={handleDelete}>Delete</button></td>
+        </tr>)
+}
+export default ApplicationRow
