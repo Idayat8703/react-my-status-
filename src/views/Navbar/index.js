005f778de14fff529ec07eb19f3e979f746f8c24
Idayat <github email address>
2017-06-04 12:36:11 -0400

    add Navbar props

--- src/views/Navbar/index.js
+++ src/views/Navbar/index.js
@@ -6,3 +6,20 @@ type Props = {
   isAuthenticated: boolean,
   logout: () => void,
 }
+class Navbar extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  static contextTypes = {
+    router: PropTypes.object,
+  }
+
+  props: Props
+
+  handleLogout() {
+    this.props.logout(this.context.router)
+  }
