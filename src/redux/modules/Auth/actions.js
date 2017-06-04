c821ca502a8d5498b297b95ebb93453419035664
Idayat <github email address>
2017-06-04 13:00:00 -0400

    add auth to reducers

--- src/redux/modules/Auth/actions.js
+++ src/redux/modules/Auth/actions.js
@@ -8,3 +8,39 @@ const authRequest = () => {
     type: 'AUTHENTICATION_REQUEST',
   }
 }
+const authSuccess = (user, token) => {
+  return {
+    type: 'AUTHENTICATION_SUCCESS',
+    user: user,
+    token: token
+  }
+}
+
+export const authFailure = (errors) => {
+  return {
+    type: 'AUTHENTICATION_FAILURE',
+    errors: errors
+  }
+}
+
+
+// async functions
+
+export const signup = (user, router) => {
+  return dispatch => {
+    dispatch(authRequest())
+    return ApiServices.post('/users', user)
+      .then(response => {
+        const { user, token } = response
+        localStorage.setItem('token', token)
+        dispatch(authSuccess(user, token))
+        dispatch(reset('signup'))
+        router.history.replace('/dashboard')
+      })
+      .catch((errors) => {
+        console.log(errors)
+        dispatch(authFailure(errors))
+        throw new SubmissionError(errors)
+      })
+  }
+}
