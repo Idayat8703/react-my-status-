20a44f168987f4a02889aedd640a8ea7a85c1ecf
Idayat <github email address>
2017-06-04 12:09:40 -0400

    user i being validated

--- src/views/components/Forms/users.js
+++ src/views/components/Forms/users.js
@@ -1,2 +1,18 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+const validate = values => {
+  const errors = {};
+
+  if (!values.username) {
+    errors.username = 'Username is required';
+  } else if (values.username.length < 2) {
+    errors.username = 'Username must be a minimum of 2 characters';
+  }
+  if (!values.password) {
+    errors.password = 'Password is required';
+  } else if (values.password.length < 8) {
+    errors.password = 'Password must be a minimum of 8 characters';
+  }
+
+  return errors;
+}
