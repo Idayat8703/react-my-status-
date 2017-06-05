4f4e6aaccf7d52258488a07d86697e10e8350132
Idayat <github email address>
2017-06-05 19:56:27 -0400

    fix css

--- src/views/Welcome/index.js
+++ src/views/Welcome/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const  Welcome = () =>
     <div>
-      <h1><span>Welcome To My Job Application Tracker</span></h1>
-      <h3><span>Please <a href='/signup'>sign up</a> or <a href='/login'>log in</a> to use</span></h3>
+      <h1><span>What's my job status? </span></h1>
+      <h3><span>Please <a href='/signup'>sign up</a> or <a href='/login'>log in</a> to see status</span></h3>
     </div>
 
 export default Welcome
